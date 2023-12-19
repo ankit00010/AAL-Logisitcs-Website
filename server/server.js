0d1509c94d41cd8fc70f0f847b34c99808a0c566
Ankit Mishra <111192702+ankit00010@users.noreply.github.com>
2023-12-19 13:25:31 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://aalogistic.vercel.app',
+  origin: '*',
   methods: ['POST'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
