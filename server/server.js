b76a6fb40b4afa3c4cde7b07ba51545581f2950f
Ankit Mishra <111192702+ankit00010@users.noreply.github.com>
2023-12-19 13:28:10 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,15 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: '*',
+  origin: function (origin, callback) {
+    // Check if the request origin is allowed
+    const allowedOrigins = ['https://aalogistic.vercel.app'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['POST'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
